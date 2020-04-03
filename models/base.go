fb0f5114ac8ccf854439538c03c80792112cc388
mstfymrtc <mstfymrtci@gmail.com>
2020-04-03 18:07:33 +0300

    add db connection file

---
+++ models/base.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var db *gorm.DB //database
+
+func init() {
+	conn, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db = conn
+	//Migarate db
+	db.Debug().AutoMigrate(&Account{}, &Contact{})
+}
+func GetDB() *gorm.DB {
+	return db
+}
