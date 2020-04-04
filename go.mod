ace637e7039e15a1f30666e4882225e7ada9be59
mstfymrtc <mstfymrtci@gmail.com>
2020-04-04 22:39:06 +0300

    set cors config

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/handlers v1.4.2 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.12
+	github.com/rs/cors v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 )
--- main.go
+++ main.go
@@ -5,25 +5,25 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mstfymrtc/go-posts-api/app"
 	"github.com/mstfymrtc/go-posts-api/controllers"
+	"github.com/rs/cors"
 	"net/http"
 	"os"
 )
 
 func main() {
 	router := mux.NewRouter()
-
+	router.Use(app.JwtAuthentication)
 	router.HandleFunc("/api/register", controllers.Register).Methods("POST")
 	router.HandleFunc("/api/authenticate", controllers.Authenticate).Methods("POST")
 	router.HandleFunc("/api/posts", controllers.GetPosts).Methods("GET")
 	router.HandleFunc("/api/posts/{id}", controllers.GetPost).Methods("GET")
 	router.HandleFunc("/api/posts", controllers.CreatePost).Methods("POST")
-
-	router.Use(app.JwtAuthentication)
+	handler := cors.Default().Handler(router)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
-	err := http.ListenAndServe(":"+port, router)
+	err := http.ListenAndServe(":"+port, handler)
 	if err != nil {
 		fmt.Println(err)
 	}
